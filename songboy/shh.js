95ca687f7fa2f097e6a8701a5d5681b823db18fa
mozinomer <mozinomer12@gmail.com>
2021-03-18 14:26:56 +0500

    asd

--- songboy/shh.js
+++ songboy/shh.js
@@ -14,4 +14,42 @@ $('.testimonialsSlider').slick({
 		}
 	}
 	]
-})
+});
+$(window).on('scroll', function () {
+	var scrolled = $(this).scrollTop();
+	$('.bannerContainerHomepahe .container').css({
+		'transform': 'translate3d(0, ' + -(scrolled * 0.40) + 'px, 0)',
+		'opacity': 1 - scrolled / 400
+	});
+});
+
+$(document).ready(function() {
+	$(".title").lettering();
+});
+ var flag = true;
+function animation() {
+	var title1 = new TimelineMax();
+	title1.to(".button", 0, {visibility: 'hidden', opacity: 0})
+	title1.staggerFromTo(".title span", 0.5, 
+		{ opacity: 1, height: 0},
+		{ opacity: 1, height: 40}, 0.05);
+	title1.to(".button", 0.2, {visibility: 'visible' ,opacity: 1})
+}
+
+$(window).scroll(function() {
+              //         for flag  
+      var top_of_element = $(".contentStartAfterBanner").offset().top;
+      var bottom_of_element = $(".contentStartAfterBanner").offset().top + $(".contentStartAfterBanner").outerHeight();
+      var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+      var top_of_screen = $(window).scrollTop();
+      if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && flag){
+      	animation();
+      	console.log('in h')
+      	flag = false;
+      } else {}
+
+
+});
+
+
+
